e977b38b04255d35d8a22f284bc2a81ef4294abe
ProntoJulian <julian.prontomowers@gmail.com>
2024-05-17 18:34:47 +0000

    Se intenta solucional el error

--- app.js
+++ app.js
@@ -5,53 +5,13 @@ const cookieParser = require('cookie-parser');
 const { engine } = require('express-handlebars');
 const methodOveride = require("method-override")
 const session = require("express-session")
-let exphbs = require('express-handlebars');
 const flash = require('express-flash');
 const passport = require('passport');
-
-
-
-
-const routerCompanies = require("./routes/RoutesDBCRUD/companies");
-const routerFeeds = require("./routes/RoutesDBCRUD/feeds");
-const routerRoles = require("./routes/RoutesDBCRUD/roles");
-const routerUsers = require("./routes/RoutesDBCRUD/users");
-const routerUserFeeds = require("./routes/RoutesDBCRUD/userFeed");
-const routerAuth = require("./routes/auth.js");
-
-// Import routers from the routes directory
-const routerMerchant = require("./routes/Merchant/googleMerchant");
-const routerImages = require("./routes/Merchant/images");
-const routerOrders = require("./routes/Merchant/orders");
-const routerProducts = require("./routes/Merchant/products");
-const routerWebHooks = require("./routes/Merchant/webHooks");
 
 const database = require(path.join(__dirname, 'databases', 'prontoWebDB'));
 const GoogleAPI = require(path.join(__dirname, 'api', 'googleMerchantAPI'));
 
-// Setup express to automatically parse JSON bodies
-app.use(express.json());
-app.use(cookieParser());
-
-// Register routers to handle different route prefixes
-app.use(routerOrders);
-app.use(routerProducts);
-app.use(routerMerchant);
-app.use(routerImages);
-app.use(routerWebHooks);
-app.use(routerAuth);
-
-
-app.use(routerCompanies);
-app.use(routerFeeds);
-app.use(routerRoles);
-app.use(routerUsers);
-app.use(routerUserFeeds);
-app.use(routerAuth);
-
-
-// Settings
-
+// Setting up Handlebars
 app.set('views', path.join(__dirname,"src","views"));
 app.engine('.hbs', engine({
   extname: '.hbs',
@@ -61,13 +21,11 @@ app.engine('.hbs', engine({
 }));
 app.set('view engine', '.hbs');
 
-
 // Middlewares
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Important to be before route definitions
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 app.use(methodOveride('_method'));
-
 app.use(session({
     secret: 'mysecretapp',
     resave: true,
@@ -78,7 +36,6 @@ app.use(passport.session());
 app.use(flash());
 
 // Global Variables
-
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
@@ -86,23 +43,39 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
 
 // Static Files
+app.use(express.static(path.join(__dirname,"src","public")));
 
-app.use(express.static(path.join(__dirname,"src","public")))
+// Importing routers
+const routerCompanies = require("./routes/RoutesDBCRUD/companies");
+const routerFeeds = require("./routes/RoutesDBCRUD/feeds");
+const routerRoles = require("./routes/RoutesDBCRUD/roles");
+const routerUsers = require("./routes/RoutesDBCRUD/users");
+const routerUserFeeds = require("./routes/RoutesDBCRUD/userFeed");
+const routerAuth = require("./routes/auth.js");
+const routerMerchant = require("./routes/Merchant/googleMerchant");
+const routerImages = require("./routes/Merchant/images");
+const routerOrders = require("./routes/Merchant/orders");
+const routerProducts = require("./routes/Merchant/products");
+const routerWebHooks = require("./routes/Merchant/webHooks");
+
+// Register routers
+app.use(routerOrders);
+app.use(routerProducts);
+app.use(routerMerchant);
+app.use(routerImages);
+app.use(routerWebHooks);
+app.use(routerAuth);
+app.use(routerCompanies);
+app.use(routerFeeds);
+app.use(routerRoles);
+app.use(routerUsers);
+app.use(routerUserFeeds);
+app.use(routerAuth);
 
 // Server is listening
-
-
-// Define the port number as environment variable or default to 443
 const PORT = process.env.PORT || 8443;
-
-// Start the server and log the server address
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
