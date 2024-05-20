22ba68b10cd9f48f400c050f93f6c44bad8b8766
ProntoJulian <julian.prontomowers@gmail.com>
2024-05-20 13:53:36 +0000

    Intentando implementar la autenticación

--- middleware/index.js
+++ middleware/index.js
@@ -5,7 +5,9 @@ function authenticateToken(req, res, next) {
     //const authHeader = req.headers['authorization'];
     //const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.status(401).json({ message: "No token provided" });
+    if (!token) {
+        res.redirect("/login")
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err){
