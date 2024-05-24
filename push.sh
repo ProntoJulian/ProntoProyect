a16d7767f6788545d01d084fb918b569ca3cf011
ProntoJulian <julian.prontomowers@gmail.com>
2024-05-24 15:36:06 +0000

    Se arregla el error de Roles de que no se puede abrir el modal

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Verifica que se haya proporcionado un mensaje de commit
+if [ -z "$1" ]; then
+  echo "Error: Debes proporcionar un mensaje para el commit."
+  echo "Uso: ./git_push.sh \"Tu mensaje de commit\""
+  exit 1
+fi
+
+# Guarda el mensaje del commit
+commit_message="$1"
+
+# Muestra el estado de git
+git status
+
+# Agrega todos los cambios
+git add .
+
+# Hace el commit con el mensaje proporcionado
+git commit -m "$commit_message"
+
+# Empuja los cambios a la rama main
+git push origin main
+
+echo "Cambios empujados a la rama main con éxito."
