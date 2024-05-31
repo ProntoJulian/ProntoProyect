61a12170105db9b1debfaa680f4a3d042cd0b41b
ProntoJulian <julian.prontomowers@gmail.com>
2024-05-31 18:36:06 +0000

    Se hacen cambios para la aparicion de un modal al sincronizar

--- routes/Merchant/webHooks.js
+++ routes/Merchant/webHooks.js
@@ -105,6 +105,8 @@ routerWebHooks.post("/createdProduct/:feedID", async (req, res) => {
     const { feedID } = req.params;
     const feed = await fetchOneFromTable('feeds', feedID, 'feed_id');
 
+    console.log("Feed: ", feed)
+
     const storeHash = feed.store_hash;
     const accessToken = feed.x_auth_token;
     const privateKey = feed.private_key;
