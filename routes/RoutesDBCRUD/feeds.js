e28e54caac58db24d234dd84ea82f066d23ed7e4
ProntoJulian <julian.prontomowers@gmail.com>
2024-06-18 14:51:57 +0000

    Se agrega una nueva ventana para crear feeds

--- routes/RoutesDBCRUD/feeds.js
+++ routes/RoutesDBCRUD/feeds.js
@@ -101,7 +101,7 @@ routerFeeds.get("/feeds/updateFeed/:feedId", authenticateToken, async (req, res)
     }
 });
 
-routerFeeds.get("app/feeds/createFeed", authenticateToken, async (req, res) => {
+routerFeeds.get("/app/feeds/createFeed", authenticateToken, async (req, res) => {
 
     console.log("Funciona?")
 
